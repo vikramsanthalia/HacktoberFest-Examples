0bfb7850e76524775d384f4b2d3b448740ff4a6d
Vikram Santhalia <vikram.san@PW-C02VN0ZTG8WN.local>
2018-10-05 14:34:01 +0530

    Hello World in GO

---
+++ helloworld.go
@@ -0,0 +1,24 @@
+//declare the package
+package main
+
+//import the fmt package
+import "fmt"
+
+//main function to run the app
+func main() {
+    fmt.Println("hello world")
+}
+
+
+// To run the program, put the code in hello-world.go and use go run.
+// $ go run hello-world.go
+// hello world
+
+// To build our programs into binaries
+// $ go build hello-world.go
+// $ ls
+// hello-world	hello-world.go
+
+// To execute the built binary directly
+// $ ./hello-world
+// hello world
